c07a4acd535ba3a23f732ad502d86725c30b7025
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-12-01 22:09:41 +0100

    Create Level class that stores data about Level with a static method that reads the data from txt file

---
+++ Labirynt/Elements/Level.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Labirynt.Elements
+{
+    public class Level
+    {
+        public Coordinate StartPosition;
+        public List<Coordinate> ObstaclePositions;
+        public Coordinate EndPosition;
+
+        public static Level FromTxt(string levelName)
+        {
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/Levels/" + levelName;
+            var lines = File.ReadAllLines(path);
+
+            Level level = new Level();
+            level.ObstaclePositions = new List<Coordinate>();
+
+            //First line is for spawning, last one is end point and the rest is reserved for obstacles
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int[] cords = lines[i].Split(';').Select(Int32.Parse).ToArray();
+
+                if(i == 0)
+                {
+                    level.StartPosition = new Coordinate(cords[0], cords[1]);
+                }
+                else if(i == lines.Length-1)
+                {
+                    level.EndPosition = new Coordinate(cords[0], cords[1]);
+                }
+                else
+                  level.ObstaclePositions.Add(new Coordinate(cords[0], cords[1]));
+            }
+
+            return level;
+        }
+    }
+}
