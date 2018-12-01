fc119d07a1c15b5c3e900fb5199ff7a96219ab43
Mikołaj Szymański <mikszym95@gmail.com>
2018-12-01 20:55:20 +0100

    Initial Commit

---
+++ Labirynt/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Labirynt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Player player;
+        public MainWindow() => InitializeComponent();
+    }
+}
---
+++ Labirynt/Player/PlayerRepresentation.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Shapes;
+
+namespace Labirynt.Player
+{
+    public class PlayerRepresentation
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Rectangle GraphicRepresentation { get; }
+        public PlayerRepresentation(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
