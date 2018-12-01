6094433c930a20bc774210914852a28522a40e4b
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-12-01 21:30:10 +0100

    Add Player class that allows to move orange square

---
+++ Labirynt/Elements/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Labirynt.Elements
+{
+    public class Player
+    {
+        public Rectangle PlayerRectangle;
+        private int boardWidth;
+        private int boardHeight;
+
+        public Player(int boardWidthParam, int boardHeightParam)
+        {
+            PlayerRectangle = new Rectangle();
+            PlayerRectangle.Fill = Brushes.Orange;
+
+            this.boardWidth = boardWidthParam;
+            this.boardHeight = boardHeightParam;
+        }
+
+        public void Move(int dirX,int dirY)
+        {
+            int rowToSet = Grid.GetRow(PlayerRectangle) + dirY;
+            int colToSet = Grid.GetColumn(PlayerRectangle) + dirX;
+
+            if (colToSet >= 0 && colToSet < boardWidth)
+                Grid.SetColumn(PlayerRectangle, Grid.GetColumn(PlayerRectangle) + dirX);
+
+            if (rowToSet >= 0 && rowToSet < boardHeight)
+                    Grid.SetRow(PlayerRectangle, Grid.GetRow(PlayerRectangle) + dirY);
+        }
+    }
+}
--- Labirynt/MainWindow.xaml.cs
+++ Labirynt/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using Labirynt.Player;
+﻿using Labirynt.Elements;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Labirynt
 {
@@ -12,11 +13,41 @@ namespace Labirynt
         private int BoardWidth = 16;
         private int BoardHeight = 16;
 
+        private Player player;
+
         public MainWindow()
         {
             InitializeComponent();
-
             InitializeBoard();
+            InitializePlayer();
+
+            this.KeyDown += MainWindow_KeyDown;
+        }
+
+        private void InitializePlayer()
+        {        
+            player = new Player(BoardWidth,BoardHeight);
+            Grid.Children.Add(player.PlayerRectangle);
+        }
+
+        private void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if(e.Key == Key.W)
+            {
+                player.Move(0, -1);
+            }
+            else if(e.Key == Key.A)
+            {
+                player.Move(-1, 0);
+            }
+            else if (e.Key == Key.S)
+            {
+                player.Move(0, 1);
+            }
+            else if (e.Key == Key.D)
+            {
+                player.Move(1, 0);
+            }
         }
 
         private void InitializeBoard()
