5c53925db26afd9a54fdca94fd7730d5486fa512
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-12-01 22:08:53 +0100

    Create class that represents Obstacle in the Labirynth

---
+++ Labirynt/Elements/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Labirynt.Elements
+{
+    public class Obstacle
+    {
+        public Rectangle obstacleRect;
+
+        public int X { get; }
+        public int Y { get; }
+
+        public Obstacle(int x,int y)
+        {
+            obstacleRect = new Rectangle();
+            obstacleRect.Fill = Brushes.Black;
+
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
