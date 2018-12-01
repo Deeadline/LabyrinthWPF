7f3b3554c8442a2db75ad95d605f445dfce4e0c6
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-12-01 22:09:57 +0100

    Prepare sample map from txt file

--- Labirynt/MainWindow.xaml.cs
+++ Labirynt/MainWindow.xaml.cs
@@ -15,10 +15,15 @@ namespace Labirynt
 
         private Player player;
 
+        private Level CurrentLevel;
+
         public MainWindow()
         {
             InitializeComponent();
             InitializeBoard();
+            CurrentLevel = Level.FromTxt("SampleLevel.txt");
+
+            InitializeObstacles();
             InitializePlayer();
 
             this.KeyDown += MainWindow_KeyDown;
@@ -28,6 +33,21 @@ namespace Labirynt
         {        
             player = new Player(BoardWidth,BoardHeight);
             Grid.Children.Add(player.PlayerRectangle);
+            Grid.SetColumn(player.PlayerRectangle, CurrentLevel.StartPosition.X);
+            Grid.SetRow(player.PlayerRectangle, CurrentLevel.StartPosition.Y);
+        }
+
+        private void InitializeObstacles()
+        {
+            for (int i = 0;i < CurrentLevel.ObstaclePositions.Count;i++)
+            {
+            
+                Obstacle obs = new Obstacle(CurrentLevel.ObstaclePositions[i].X,
+                    CurrentLevel.ObstaclePositions[i].Y);
+                Grid.Children.Add(obs.obstacleRect);
+                Grid.SetColumn(obs.obstacleRect, obs.X);
+                Grid.SetRow(obs.obstacleRect, obs.Y);
+            }
         }
 
         private void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
@@ -62,7 +82,5 @@ namespace Labirynt
                 Grid.ColumnDefinitions.Add(new ColumnDefinition());
             }
         }
-
-        
     }
 }
